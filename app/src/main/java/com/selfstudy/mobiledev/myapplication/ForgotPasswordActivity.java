eb476369fb40c355efd6efaacc1f487bee0b3e1f
Myat Thiri Maung <maungmyatthiri@gmail.com>
2025-02-14 15:28:04 +0700

    Add Forgot Password Screen

--- app/src/main/java/com/selfstudy/mobiledev/myapplication/ForgotPasswordActivity.java
+++ app/src/main/java/com/selfstudy/mobiledev/myapplication/ForgotPasswordActivity.java
@@ -1,6 +1,5 @@
 package com.selfstudy.mobiledev.myapplication;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -10,22 +9,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+public class ForgotPasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_forgot_password);
 
     }
 
     public void finishCurrentActivity(View view) {
         finish();
-    }
-
-    public void switchToForgotPassword(View view) {
-        Intent intentForgotPassword = new Intent(this, ForgotPasswordActivity.class);
-        startActivity(intentForgotPassword);
     }
 }
